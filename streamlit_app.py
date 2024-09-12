cbb60d81b4a1a58df5eea2ef852ff5b029c46d14
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 16:51:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("🪐 NASA Machine Learning App 🪐")
+st.title("🪐 NASA ML Application 🪐")
 st.info('This application will allow you to do EDA and predict the threat to Earth thanks to machine learning models')
