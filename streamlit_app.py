f4cf83e9c20551db2f8cc30f113617a04f89f92b
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 17:35:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("🪐 NASA's Space Object Tracker 🪐")
+st.title("🪐 NASA ML Application 🪐")
 
 st.info(
     """
-    **Explore the Wonders and Dangers of Space!**  
+    **Explore the Wonders and Dangers of Space!**\\
     The universe is full of celestial bodies, but some come perilously close to Earth. These are known as **N.E.O.s - Near Earth Objects**. 
     While most of these space wanderers harmlessly pass by, a few are classified as *hazardous* by NASA, posing potential threats to our planet.  
     Dive into the data and uncover the history of these fascinating objects, from their first recorded observation in 1910 to the present day.
