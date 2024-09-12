8dde475f2bd6c34380050219c20419453fdf6402
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 17:34:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("🪐 NASA ML Application 🪐")
-st.write("Author: Mateusz Walo")
-st.info("There are many dangerous bodies in space, one of them is N.E.O. - *Nearest Earth Objects*. Some such bodies really pose a danger to the planet Earth, NASA classifies them as *is_hazardous*.")
-df=pd.read_csv("https://raw.githubusercontent.com/mateuszwalo/NASA_app/master/Nasa_clean_v2.csv")
+st.title("🪐 NASA's Space Object Tracker 🪐")
 
-with st.expander("Data"):
-  st.write("**This dataset contains ALL NASA observations of similar objects from 1910 to 2024**")
-  df
+st.info(
+    """
+    **Explore the Wonders and Dangers of Space!**  
+    The universe is full of celestial bodies, but some come perilously close to Earth. These are known as **N.E.O.s - Near Earth Objects**. 
+    While most of these space wanderers harmlessly pass by, a few are classified as *hazardous* by NASA, posing potential threats to our planet.  
+    Dive into the data and uncover the history of these fascinating objects, from their first recorded observation in 1910 to the present day.
+    """
+)
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/mateuszwalo/NASA_app/master/Nasa_clean_v2.csv")
+
+with st.expander("🔍 View NASA's Data Records"):
+    st.write("**Explore NASA's extensive database, documenting every recorded N.E.O. from 1910 to 2024.**")
+    st.dataframe(df)
