c2a6e1cf058a0a8b27035ffa660f6832759c2ea0
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 16:59:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("🪐 NASA ML Application 🪐")
 st.info('This application will allow you to do EDA and predict the threat to Earth thanks to machine learning models')
+
+df=pd.read_csv("Nasa_clean.csv")
+df.head()
