01cf0482d2359ac80fc7e512b5c76fce657311ad
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 17:02:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ st.title("🪐 NASA ML Application 🪐")
 st.info('This application will allow you to do EDA and predict the threat to Earth thanks to machine learning models')
 
 df=pd.read_csv("https://raw.githubusercontent.com/mateuszwalo/NASA_app/master/Nasa_clean.csv")
-df.head()
+df
