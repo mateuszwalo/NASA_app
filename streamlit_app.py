bcc774bc886369d629b06ae2f7375838e59170a7
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-12 17:31:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.title("🪐 NASA ML Application 🪐")
-st.info('This application will allow you to do EDA and predict the threat to Earth thanks to machine learning models')
+st.write("Author: Mateusz Walo")
+st.info("There are many dangerous bodies in space, one of them is N.E.O. - "Nearest Earth Objects". Some such bodies really pose a danger to the planet Earth, NASA classifies them as "is_hazardous".")
 df=pd.read_csv("https://raw.githubusercontent.com/mateuszwalo/NASA_app/master/Nasa_clean_v2.csv")
 
 with st.expander("Data"):
-  st.write("**Overwiev of dataset from NASA**")
+  st.write("**This dataset contains ALL NASA observations of similar objects from 1910 to 2024**")
   df
